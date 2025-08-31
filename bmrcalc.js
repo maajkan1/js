c3075a1ad6a4323431fa11b96b11903232814006
maajkan1 <maajkan@live.se>
2025-08-31 11:21:17 +0200

    lade till kaloriuträknar logiken. Ska finjustera sen

---
+++ bmrcalc.js
@@ -0,0 +1,15 @@
+let age = 25;
+let height = 156;
+let weight = 70;
+
+value = "woman";
+
+if (value = "man")
+{
+   let bmrman =  66.47 + (13.75 * weight) + (5.003 * height) - (6.775 * age)
+    console.log(bmrman)
+}
+else {
+    let bmrwoman = 655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age);
+    console.log(bmrwoman);
+}
